4cf89657ab0f3ccf9022dc5b81a4313e36e7b9a7
satoshi-n <sato171786@gmail.com>
2017-11-13 20:45:35 +0900

    ResponseEntity,Resourceでダウンロード

--- src/main/java/com/example/login/presentation/controller/DownloadRestController.java
+++ src/main/java/com/example/login/presentation/controller/DownloadRestController.java
@@ -1,9 +1,16 @@
 package com.example.login.presentation.controller;
 
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 
 @RestController
 @RequestMapping("/api/download/")
@@ -13,4 +20,19 @@ public class DownloadRestController {
     public String download() {
         return "<a><b>content</b></a>";
     }
+
+    @GetMapping("byte")
+    public ResponseEntity<byte[]> byteDownload() throws UnsupportedEncodingException {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("Content-Type", "text/csv; charset=MS932");
+        httpHeaders.setContentDispositionFormData("filename", "こんにちは.csv", StandardCharsets.UTF_8);
+        return new ResponseEntity<>("あ,い,う,え,お".getBytes("MS932"), httpHeaders, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "build", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public FileSystemResource fileSystemResource() {
+        return new FileSystemResource("build.gradle");
+    }
+
+
 }
